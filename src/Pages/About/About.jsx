c9c0f85dfed73413148d2e422e00c535f3f30f9f
VANSH JAIN <vanshj9971@gmail.com>
2026-01-16 00:45:05 +0530

    Fix formatting in About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,7 +6,7 @@ const testimonials = [
         name: 'Vansh Jain',
         role: 'Fronted Web Developer',
         content:
-            'A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.',
+            'A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.'
         
     }
 ];
